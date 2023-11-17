907ab271eecc7c13907d24cfb74217256e6987d5
Humza Khan <humza.khan890@gmail.com>
2023-11-17 06:38:27 +0000

    Commented code

--- JS/main.js
+++ JS/main.js
@@ -5,22 +5,30 @@ function GetYearCard(year)
 
 function CreateYearCard(year)
 {
+    // Get template and clone
     const yearsDiv = document.querySelector("#YearsDiv")
     const template = document.querySelector("#YearCardTemplate")
     const clone = template.content.cloneNode(true)
+
+    // Set year and card Id
     clone.querySelector("h3").textContent = year
     clone.querySelector("#Cards").setAttribute("id", year + "-cards")
+
+    // Add to years div
     yearsDiv.append(clone)
 }
 
 
 function CreateProjectCard(yearCard, project, projectId)
 {
+    // Get template and clone
     const template = document.querySelector("#ProjectCardTemplate")
     const clone = template.content.cloneNode(true)
 
+    // Create project unique ID (Used for carousel)
     var projectId = "Project" + projectId
 
+    // Get carousel and add media
     var carouselInner = clone.querySelector(".carousel-inner")
     var carouselItem = document.createElement("div")
     carouselItem.setAttribute("class", "carousel-item active ratio ratio-16x9")
@@ -30,14 +38,18 @@ function CreateProjectCard(yearCard, project, projectId)
         carouselItem.append(img)
         carouselInner.append(carouselItem)
     }
+
+    // Set up carousel unique ID
     clone.querySelector(".carousel").setAttribute("id", projectId)
     clone.querySelector(".carousel-control-prev").setAttribute("data-bs-target", projectId)
     clone.querySelector(".carousel-control-next").setAttribute("data-bs-target", projectId)
 
+    // Set title, subtitle and description
     clone.querySelector(".card-title").textContent = project["DisplayName"]
     clone.querySelector(".card-subtitle").textContent = project["Subtitle"]
     clone.querySelector(".card-text").textContent = project["Description"]
 
+    // Set links
     for (var linkKey in project["Links"])
     {
         var linkElement =  document.createElement("a")
@@ -48,6 +60,7 @@ function CreateProjectCard(yearCard, project, projectId)
         clone.querySelector(".links").append(linkElement)
     }
 
+    // Set tags
     for (var tag in project["Tags"])
     {
         var tagElement = document.createElement("Span")
@@ -56,12 +69,13 @@ function CreateProjectCard(yearCard, project, projectId)
         clone.querySelector(".card-footer").append(tagElement)
         clone.querySelector(".card-footer").append(" ")
     }
-
+ 
+    // Add to the year card
     yearCard.append(clone)
 }
 
 
-
+// Initialise
 var projectId = 0
 for (var key in Projects) {
     projectId++
@@ -76,12 +90,4 @@ for (var key in Projects) {
     }
     
     CreateProjectCard(yearCard, project, projectId)
-
-    for (var key2 in project) 
-    {
-        console.log(key2 + " : " + project[key2])
-
-    } 
 }
-// How to write inside HTML element
-//document.getElementById("ProjectYears").innerHTML
