099be4df95aa8a4f920ad76c8d7a28c7ed0be12e
Humza Khan <humza.khan890@gmail.com>
2023-11-17 02:12:47 +0000

    Created projects file

---
+++ JS/projects.js
@@ -0,0 +1,15 @@
+var Projects = 
+{
+    "Happy Unicorn Adventure" : 
+    {
+        "DisplayName" : "🦄Happy Unicorn Adventure",
+        "Year" : 2023,
+        "Subtitle" : "Present",
+        "Media" : "../../Media/Happy_Unicorn_Adventure",
+        "Description" : "A unicorn themed game targeted towards young audiences (under 10). Contains 30 minigames ranging from avoiding dragon flames 🐉, flipping a coin 🍀, completing obstacles (AKA Obbies) and much more. There are different game modes like Party mode and Elimination mode.",
+        "Links" : 
+        {
+            "Roblox" : "https://www.roblox.com/games/10966411732/Happy-Unicorn-Adventure-Smoother-UI#!/about"
+        }
+    }
+}
