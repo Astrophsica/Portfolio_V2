663430db2568d112885620809efd57232511efc8
Humza Khan <humza.khan890@gmail.com>
2023-11-22 12:34:27 +0000

    Created tags JS file

---
+++ JS/tags.js
@@ -0,0 +1,48 @@
+Tags = 
+{
+    ["Lua"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Primary"
+    },
+    ["C#"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Primary"
+    },
+    ["Web (HTML | CSS | JavaScript)"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Primary"
+    },
+    ["Roblox"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Secondary"
+    },
+    ["Unity"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Secondary"
+    },
+    ["University"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Secondary"
+    },
+    ["Open Source"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Secondary"
+    },
+    ["OpenGL"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Secondary"
+    },
+    ["Shaders"] : 
+    {
+        ["Enabled"] : false,
+        ["Type"] : "Secondary"
+    },
+}
