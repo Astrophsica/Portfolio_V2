9e7c420fcf54e67ad5a732bf3cde63bf485ecc20
Astrophsica <humza.khan890@gmail.com>
2023-11-23 04:50:28 +0000

    Created icon js file

---
+++ JS/icons.js
@@ -0,0 +1,7 @@
+const Icons = 
+{
+    ["Roblox"]: "Media/Icons/Roblox.svg",
+    ["YouTube"]: "Media/Icons/YouTube.svg",
+    ["Website"]: "Media/Icons/Website.svg",
+    ["GitHub"]: "Media/Icons/GitHub.svg"
+}
