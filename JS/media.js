63b2756576d595b14b80d5abb252f39accbf0348
Humza Khan <humza.khan890@gmail.com>
2024-03-07 20:01:37 +0000

    Added media js file to store media with LOD

---
+++ JS/media.js
@@ -0,0 +1,133 @@
+const Media = 
+{
+    "HappyUnicornAdventure/Thumbnail" : 
+    [
+        "Media/Projects/Happy_Unicorn_Adventure/HappyUnicornAdventure.png"
+    ],
+
+    "HappyUnicornAdventure/LuckMinigame" : 
+    [
+        "Media/Projects/Happy_Unicorn_Adventure/Luck_Minigame-LowRes.gif",
+        "Media/Projects/Happy_Unicorn_Adventure/Luck_Minigame.gif"
+    ],
+
+    "HappyUnicornAdventure/LizardMinigame" : 
+    [
+        "Media/Projects/Happy_Unicorn_Adventure/Lizard_Minigame-LowRes.gif",
+        "Media/Projects/Happy_Unicorn_Adventure/Lizard_Minigame.gif"
+    ],
+
+    "HappyUnicornAdventure/Shop" : 
+    [
+        "Media/Projects/Happy_Unicorn_Adventure/Shop-LowRes.gif",
+        "Media/Projects/Happy_Unicorn_Adventure/Shop.gif"
+    ],
+
+    "HappyUnicornAdventure/Dragon" : 
+    [
+        "Media/Projects/Happy_Unicorn_Adventure/Dragon-LowRes.gif",
+        "Media/Projects/Happy_Unicorn_Adventure/Dragon.gif"
+    ],
+
+
+
+    "PortfolioSite/Thumbnail" : 
+    [
+        "Media/Projects/Portfolio/Spiderman_Meme.jpg",
+    ],
+
+
+
+    "AlienAttack/Thumbnail" : 
+    [
+        "Media/Projects/Alien_Attack/AlienAttack.png",
+    ],
+
+    "AlienAttack/Placement" : 
+    [
+        "Media/Projects/Alien_Attack/Alien_Attack_Placement-LowRes.gif",
+        "Media/Projects/Alien_Attack/Alien_Attack_Placement.gif"
+    ],
+
+    "AlienAttack/Wall" : 
+    [
+        "Media/Projects/Alien_Attack/Alien_Attack_Wall-LowRes.gif",
+        "Media/Projects/Alien_Attack/Alien_Attack_Wall.gif"
+    ],
+
+    "AlienAttack/Turret" : 
+    [
+        "Media/Projects/Alien_Attack/Alien_Attack_Turret-LowRes.gif",
+        "Media/Projects/Alien_Attack/Alien_Attack_Turret.gif"
+    ],
+
+
+
+    "GoKartKit/Thumbnail" : 
+    [
+        "Media/Projects/Go-Kart_Kit/Go-Kart_Kit.png"
+    ],
+
+
+
+    "AutomaticTrainPrototype/Thumbnail" : 
+    [
+        "Media/Projects/Automatic_Train_Prototype/AutomaticTrainPrototype.png"
+    ],
+
+
+
+    "3DGraphics/Thumbnail" : 
+    [
+        "Media/Projects/3D_Graphics/3D_Graphics.png"
+    ],
+
+
+
+    "2DGraphics/Thumbnail" : 
+    [
+        "Media/Projects/2D_Graphics_HTML5/2D_Graphics.png"
+    ],
+
+
+
+    "GoKartingXtreme/Thumbnail" : 
+    [
+        "Media/Projects/Go-Karting_Xtreme/GKX.png"
+    ],
+
+
+
+    "ThreeThingGameJam/Thumbnail" : 
+    [
+        "Media/Projects/Three_Thing_Game/Three_Thing_Game.png"
+    ],
+
+
+
+    "ThreeThingGameJam/Thumbnail" : 
+    [
+        "Media/Projects/Three_Thing_Game/Three_Thing_Game.png"
+    ],
+
+
+
+    "Rosebridge/Thumbnail" : 
+    [
+        "Media/Projects/Rosebridge/Rosebridge.jpg"
+    ],
+
+
+
+    "PizzaPlaceJobSystem/Thumbnail" : 
+    [
+        "Media/Projects/Pizza_Place_Job_System/PizzaPlace.png"
+    ],
+
+
+
+    "AirlineCheckInSystem/Thumbnail" : 
+    [
+        "Media/Projects/Airline_Check-In_System/Airline_Check-In_System.png"
+    ],
+}
