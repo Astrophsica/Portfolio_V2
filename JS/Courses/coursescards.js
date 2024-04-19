67d0419bca199f5fdf5c30f5b35446b65f5621d7
Humza Khan <humza.khan890@gmail.com>
2024-04-19 22:44:31 +0100

    Created course cards

--- JS/Courses/coursescards.js
+++ JS/Courses/coursescards.js
@@ -1,14 +1,30 @@
 import { LodLoadMedia } from "../mediamanager.js"
 
 const apiUrl = "https://api.astrophsicadev.com/courses"
+const coursesCard = document.querySelector("#Cards")
 
-function CreateCoursesCard(courseJson)
+function CreateCourseCard(courseData)
 {
+    console.log(courseData)
 
+        // Get template and clone
+        const template = document.querySelector("#CourseCardTemplate")
+        const clone = template.content.cloneNode(true)
+
+        clone.querySelector(".card-title").textContent = courseData["course_header"]
+        clone.querySelector(".card-subtitle").textContent = courseData["dates_card"]
+        clone.querySelector(".card-text").textContent = courseData["brief_description"]
+
+        coursesCard.append(clone)
 }
 
-
-
+function ProcessCoursesData(coursesData)
+{
+    for (var courseIndex = 0; courseIndex < coursesData.length; courseIndex++)
+    {
+        CreateCourseCard(coursesData[courseIndex])
+    }
+}
 
 
 // Initialise
@@ -20,7 +36,7 @@ fetch(apiUrl)
         return response.json();
     })
     .then(data => {
-        console.log(JSON.stringify(data, null, 2))
+        ProcessCoursesData(data)
     })
 
 
